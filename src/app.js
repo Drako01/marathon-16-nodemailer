6dc881c6dc3afe112a6cb469d574ab9583db610f
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-07-01 10:44:40 -0500

    feat: sending email from gmail

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import appRoute from './routers/app.route.js'
 
 const app = express()
+app.use(express.json())
 
 app.use('/api', appRoute)
 app.listen(8080, () => console.log('Server Up!'))
--- src/controllers/app.controller.js
+++ src/controllers/app.controller.js
@@ -1,4 +1,6 @@
 import nodemailer from 'nodemailer'
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const signup = async (req, res) => {
     let testAccount = await nodemailer.createTestAccount()
@@ -28,6 +30,29 @@ export const signup = async (req, res) => {
     }
 }
 
-export const getbill = (req, res) => {
-    res.status(201).json({ message: 'Getbill successfully...'})
+export const getbill = async (req, res) => {
+    const emailUser = req.body.email
+    let config = {
+        service: 'gmail',
+        auth: {
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_PASS
+        }
+    }
+    let transporter = nodemailer.createTransport(config)
+    let message = {
+        from: process.env.GMAIL_USER,
+        to: emailUser,
+        subject: 'Compra realizada',
+        html: '<h1>gracias por tu compra!</h1>'
+    }
+
+    try {
+        const info = await transporter.sendMail(message)
+        res.status(201).json({ 
+            message: 'Email enviado..'
+        })
+    } catch(err) {
+        res.status(500).json({ error: err.message })
+    }
 }
